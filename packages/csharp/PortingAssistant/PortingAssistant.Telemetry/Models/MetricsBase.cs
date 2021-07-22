ee25d40ee09f8aa8ea9fe502e81935abffc00299
Sai Kiran Akula <saakla@amazon.com>
2021-07-22 13:05:15 -0700

    # Title: Summary, imperative, start with prefix build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test, don't end with a period, # refer https://github.com/aws/porting-assistant-dotnet-ui/blob/develop/CONTRIBUTING.md for more details # No more than 50 chars. #### 50 chars is here: fix|feat|test|docs|refactor|build|chore|perf|revert|style:
    # Remember blank line between title and body.
    # Body: Explain *what* and *why* (not *how*). Include github issue id #.
    # Explain the how you change your codefix: Add Unique machine ID to track user

--- packages/csharp/PortingAssistant/PortingAssistant.Telemetry/Models/MetricsBase.cs
+++ packages/csharp/PortingAssistant/PortingAssistant.Telemetry/Models/MetricsBase.cs
@@ -1,5 +1,7 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace PortingAssistant.Telemetry.Model
 {
@@ -12,5 +14,25 @@ namespace PortingAssistant.Telemetry.Model
         public string PortingAssistantVersion { get; set; }
         public string TargetFramework { get; set; }
         public string TimeStamp { get; set; }
+
+        /// <summary>
+        /// This property uniquely identifies the customers using porting assistant 
+        /// Auto populate this field for all the metric logs
+        /// </summary>
+        private string _customerID;
+        public string CustomerID
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_customerID))
+                {
+                    _customerID = NetworkInterface.GetAllNetworkInterfaces()
+                                         .Where(nic => nic.OperationalStatus == OperationalStatus.Up && nic.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                                         .Select(nic => nic.GetPhysicalAddress().ToString())
+                                         .FirstOrDefault();
+                }
+                return _customerID;
+            }
+        }
     }
 }
