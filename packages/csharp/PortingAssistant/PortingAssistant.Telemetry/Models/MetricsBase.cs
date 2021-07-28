727ad7015c5b2ede5e34375760f9374464d550ae
Sai Kiran Akula <87731960+saikiranakula-amzn@users.noreply.github.com>
2021-07-28 10:16:34 -0700

    fix: Add mac ID or similar stuff to track the download in each machine (#54)
    * # Title: Summary, imperative, start with prefix build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test, don't end with a period,
    # refer https://github.com/aws/porting-assistant-dotnet-ui/blob/develop/CONTRIBUTING.md for more details
    # No more than 50 chars. #### 50 chars is here:
    fix|feat|test|docs|refactor|build|chore|perf|revert|style:
    
    # Remember blank line between title and body.
    # Body: Explain *what* and *why* (not *how*). Include github issue id #.
    # Explain the how you change your codefix: Add Unique machine ID to track user
    
    * # Title: Summary, imperative, start with prefix build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test, don't end with a period,
    # refer https://github.com/aws/porting-assistant-dotnet-ui/blob/develop/CONTRIBUTING.md for more details
    # No more than 50 chars. #### 50 chars is here:
    fix|feat|test|docs|refactor|build|chore|perf|revert|style:
    
    # Remember blank line between title and body.
    # Body: Explain *what* and *why* (not *how*). Include github issue id #.
    # Explain the how you change your codefix: changed the property name to uniqueID
    
    * # Title: Summary, imperative, start with prefix build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test, don't end with a period,
    # refer https://github.com/aws/porting-assistant-dotnet-ui/blob/develop/CONTRIBUTING.md for more details
    # No more than 50 chars. #### 50 chars is here:
    fix|feat|test|docs|refactor|build|chore|perf|revert|style:
    
    # Remember blank line between title and body.
    # Body: Explain *what* and *why* (not *how*). Include github issue id #.
    # Explain the how you change your codefix: refactor to support cross platform integration

--- packages/csharp/PortingAssistant/PortingAssistant.Telemetry/Models/MetricsBase.cs
+++ packages/csharp/PortingAssistant/PortingAssistant.Telemetry/Models/MetricsBase.cs
@@ -1,8 +1,13 @@
+using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web.Helpers;
 
 namespace PortingAssistant.Telemetry.Model
 {
+    
     public class MetricsBase
     {
         [JsonConverter(typeof(StringEnumConverter))]
@@ -12,5 +17,41 @@ namespace PortingAssistant.Telemetry.Model
         public string PortingAssistantVersion { get; set; }
         public string TargetFramework { get; set; }
         public string TimeStamp { get; set; }
+
+        // This identifier will be used as default, if there are no active network interface adapters on the machine
+        private const string DefaultIdentifier = "591E6A97031144D5BADCE980EE3E51B7";
+        /// <summary>
+        /// This property uniquely identifies the customers using porting assistant 
+        /// Auto populate this field for all the metric logs
+        /// </summary>
+        private string _uniqueId;
+        public string UniqueId
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(_uniqueId) && !_uniqueId.Equals(DefaultIdentifier)) return _uniqueId;
+
+                var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces()
+                    .Where(nic => nic.NetworkInterfaceType != NetworkInterfaceType.Loopback
+                                  && (nic.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                                  && nic.Speed > 0).ToList();
+
+                // wifi network interface will always take higher precedence for retrieving physical address
+                var wifiNetworkInterface = networkInterfaces.FirstOrDefault(wi => wi.NetworkInterfaceType == NetworkInterfaceType.Wireless80211);
+                if (wifiNetworkInterface != null)
+                {
+                    _uniqueId = Crypto.SHA256(wifiNetworkInterface.GetPhysicalAddress().ToString());
+                }
+                else
+                {
+                    var ethernetInterface = networkInterfaces.LastOrDefault(ei => ei.NetworkInterfaceType == NetworkInterfaceType.Ethernet
+                                                && ei.OperationalStatus == OperationalStatus.Up && !ei.Name.Contains("Bluetooth", StringComparison.OrdinalIgnoreCase));
+                    _uniqueId = ethernetInterface != null ? Crypto.SHA256(ethernetInterface.GetPhysicalAddress().ToString()) : DefaultIdentifier;
+                }
+
+                return _uniqueId;
+            }
+        }
     }
 }
+
