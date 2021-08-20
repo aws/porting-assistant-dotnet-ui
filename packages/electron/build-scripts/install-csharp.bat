7d929e5c4bc496d9773dbcf314eec96277c7e96c
Shanshan He <shanshanhebit@gmail.com>
2021-08-19 18:50:52 -0700

    feat: Download and package PortingAssistant.Client.CLI.exe in standalone tool.

--- packages/electron/build-scripts/install-csharp.bat
+++ packages/electron/build-scripts/install-csharp.bat
@@ -2,3 +2,6 @@ rmdir /s/q netcore_build
 mkdir netcore_build
 dotnet publish ..\csharp\PortingAssistant\PortingAssistant.Api\PortingAssistant.Api.csproj -c Release -f netcoreapp3.1 -o netcore_build
 dotnet publish ..\csharp\PortingAssistant\PortingAssistant.Telemetry\PortingAssistant.Telemetry.csproj -c Release -f netcoreapp3.1 -o netcore_build 
+for /F delims^=^"^ tokens^=4 %%G in ('findstr "PortingAssistant.Client.Client"  ..\csharp\PortingAssistant\PortingAssistant.Api\PortingAssistant.Api.csproj') do @set "version=%%G"
+set cliDownloadLink="https://s3.us-west-2.amazonaws.com/aws.portingassistant.dotnet.download/nuget/flatcontainer/portingassistant.client.cli/%version%/PortingAssistant.Client.CLI.exe"
+curl %cliDownloadLink% --output netcore_build/PortingAssistant.Client.CLI.exe
