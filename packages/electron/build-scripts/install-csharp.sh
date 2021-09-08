fde526e70a8809595941fc29c9f564083d03c365
Shanshan He <shanshanhebit@gmail.com>
2021-09-08 13:18:31 -0700

    change: remove opensource cli from PA installer (#109)
    We want to wait till after PA release for Optimus integration to include the opensource CLI for Drift.

--- packages/electron/build-scripts/install-csharp.sh
+++ packages/electron/build-scripts/install-csharp.sh
@@ -3,6 +3,3 @@ rm -rf ./netcore_build/
 mkdir ./netcore_build/
 (dotnet publish ../csharp/PortingAssistant/PortingAssistant.Api/PortingAssistant.Api.csproj -c Release -f netcoreapp3.1 -o ./netcore_build/)
 (dotnet publish ../csharp/PortingAssistant/PortingAssistant.Telemetry/PortingAssistant.Telemetry.csproj -c Release -f netcoreapp3.1 -o ./netcore_build/)
-version=$(grep 'PortingAssistant.Client.Client' ../csharp/PortingAssistant/PortingAssistant.Api/PortingAssistant.Api.csproj | sed 's/.*Version="\(.*\)".*/\1/')
-cliDownloadLink="https://s3.us-west-2.amazonaws.com/aws.portingassistant.dotnet.download/nuget/flatcontainer/portingassistant.client.cli/${version}/PortingAssistant.Client.CLI.exe"
-curl $cliDownloadLink -o ./netcore_build/PortingAssistant.Client.CLI.exe
