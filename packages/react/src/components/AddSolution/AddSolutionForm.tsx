7afa129ca80bc8f1e734821cc89bb86eff3e03fa
Aniruddha Dave <aniruddhadave@gmail.com>
2022-02-16 16:27:41 -0500

    feat: check line with project guid for .vbproj

--- packages/react/src/components/AddSolution/AddSolutionForm.tsx
+++ packages/react/src/components/AddSolution/AddSolutionForm.tsx
@@ -168,11 +168,16 @@ const addSolution = async (data: Record<string, any>) => {
 const checkIfSolutionContainsVBproject = async (solutionFilename: string) => {
   var slnFileContents = "";
   try {
-    slnFileContents = await window.backend.getFileContents(solutionFilename);    
+    slnFileContents = await window.backend.getFileContents(solutionFilename);
+    var lines = slnFileContents.split("\n");
+    const pattern = /Project\("\{.*\}"\)/;
+    for (const line of lines) {
+      if (line.match(pattern)!== null && line.includes(".vbproj")) return true;
+    }
   } catch (error) {
     logError("AddSolutionForm.tsx", "Unable to read solution file.", error);
   }
-  return slnFileContents.includes(".vbproj");
+  return false;
 }
 
 export const ImportSolution = React.memo(ImportSolutionInternal);
