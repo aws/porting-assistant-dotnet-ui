8f54c4a61e3e8580c0c984200d79661d70e48559
Jon Louie <jlouie3@gmail.com>
2022-03-18 13:58:04 -0700

    fix home dir in build script

--- packages/electron/build-scripts/install-csharp.bat
+++ packages/electron/build-scripts/install-csharp.bat
@@ -4,8 +4,8 @@ mkdir netcore_build
 :: Currently, our build environment downloads the .NET 6 runtime to ~\AppData\Local\Microsoft\dotnet.
 :: When the .NET 6 runtime is included with the build environment by default, we can replace these commands
 ::  with the commented out commands below.
-~\AppData\Local\Microsoft\dotnet\dotnet publish ..\csharp\PortingAssistant\PortingAssistant.Api\PortingAssistant.Api.csproj -c Release -f net6.0 -o netcore_build
-~\AppData\Local\Microsoft\dotnet\dotnet publish ..\csharp\PortingAssistant\PortingAssistant.Telemetry\PortingAssistant.Telemetry.csproj -c Release -f net6.0 -o netcore_build 
+%userprofile%\AppData\Local\Microsoft\dotnet\dotnet publish ..\csharp\PortingAssistant\PortingAssistant.Api\PortingAssistant.Api.csproj -c Release -f net6.0 -o netcore_build
+%userprofile%\AppData\Local\Microsoft\dotnet\dotnet publish ..\csharp\PortingAssistant\PortingAssistant.Telemetry\PortingAssistant.Telemetry.csproj -c Release -f net6.0 -o netcore_build 
 
 ::dotnet publish ..\csharp\PortingAssistant\PortingAssistant.Api\PortingAssistant.Api.csproj -c Release -f net6.0 -o netcore_build
 ::dotnet publish ..\csharp\PortingAssistant\PortingAssistant.Telemetry\PortingAssistant.Telemetry.csproj -c Release -f net6.0 -o netcore_build 
