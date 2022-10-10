1e5214ba9fb38c46469689587f147e82d063ea6d
Chris Long <longachr@amazon.com>
2022-10-10 11:55:06 -0700

    test: add setup method to set telemetry info

--- packages/integration-test/src/canary.spec.ts
+++ packages/integration-test/src/canary.spec.ts
@@ -6,6 +6,7 @@ import {
   testSolutionPath,
   addSolution,
   clearSolutions,
+  setupTelemetry,
 } from "./hooks";
 import path from "path";
 import fs from "fs/promises";
@@ -17,6 +18,7 @@ describe("canary test suite", () => {
 
   beforeAll(async () => {
     app = await startApp();
+    setupTelemetry(app);
     runner = new TestRunner(app);
     await runner.setupTargetFramework();
     return app;
@@ -39,11 +41,28 @@ describe("canary test suite", () => {
   });
 
   test("run through Miniblog", async () => {
-    const solutionPath: string = path.join(testSolutionPath(), "canary", "Miniblog.Core-master", "Miniblog.Core.sln");
+    const solutionPath: string = path.join(
+      testSolutionPath(),
+      "canary",
+      "Miniblog.Core-master",
+      "Miniblog.Core.sln"
+    );
     await addSolution(app, solutionPath);
     await app.client.refresh();
-    const results = await runner.runThroughSolution(solutionPath, "inplace", "net6.0", false, false);
-    await runner.validateHighLevelResults(results, ["1 of 1", "2 of 13", "12 of 268", "0", "(21)"]);
+    const results = await runner.runThroughSolution(
+      solutionPath,
+      "inplace",
+      "net6.0",
+      false,
+      false
+    );
+    await runner.validateHighLevelResults(results, [
+      "1 of 1",
+      "2 of 13",
+      "12 of 268",
+      "0",
+      "(21)",
+    ]);
   });
 
   test("run through mvcmusicstore", async () => {
@@ -54,16 +73,45 @@ describe("canary test suite", () => {
       "sourceCode",
       "mvcmusicstore"
     );
-    const solutionPath: string = path.join(solutionFolderPath, "MvcMusicStore.sln");
+    const solutionPath: string = path.join(
+      solutionFolderPath,
+      "MvcMusicStore.sln"
+    );
     await addSolution(app, solutionPath);
     await app.client.refresh();
-    const results = await runner.runThroughSolution(solutionPath, "inplace", "net6.0", true, true);
-    await runner.validateHighLevelResults(results, ["0 of 1", "2 of 6", "50 of 85", "0", "(21)"]);
-    const controllerFolderPath: string = path.join(solutionFolderPath, "MvcMusicStore", "Controllers");
-    const getAccountController = fs.readFile(path.join(controllerFolderPath, "AccountController.cs"), "utf8");
-    const getStoreManagerController = fs.readFile(path.join(controllerFolderPath, "StoreManagerController.cs"), "utf8");
-    expect((await getAccountController).indexOf("Microsoft.AspNetCore.Mvc")).not.toBe(-1);
-    expect((await getStoreManagerController).indexOf("Microsoft.EntityFrameworkCore")).not.toBe(-1);
+    const results = await runner.runThroughSolution(
+      solutionPath,
+      "inplace",
+      "net6.0",
+      true,
+      true
+    );
+    await runner.validateHighLevelResults(results, [
+      "0 of 1",
+      "2 of 6",
+      "50 of 85",
+      "0",
+      "(21)",
+    ]);
+    const controllerFolderPath: string = path.join(
+      solutionFolderPath,
+      "MvcMusicStore",
+      "Controllers"
+    );
+    const getAccountController = fs.readFile(
+      path.join(controllerFolderPath, "AccountController.cs"),
+      "utf8"
+    );
+    const getStoreManagerController = fs.readFile(
+      path.join(controllerFolderPath, "StoreManagerController.cs"),
+      "utf8"
+    );
+    expect(
+      (await getAccountController).indexOf("Microsoft.AspNetCore.Mvc")
+    ).not.toBe(-1);
+    expect(
+      (await getStoreManagerController).indexOf("Microsoft.EntityFrameworkCore")
+    ).not.toBe(-1);
   });
 
   test("run through mixed c#/vb solution", async () => {
@@ -72,12 +120,33 @@ describe("canary test suite", () => {
       "canary",
       "MixedClassLibrary"
     );
-    const solutionPath: string = path.join(solutionFolderPath, "MixedClassLibrary.sln");
+    const solutionPath: string = path.join(
+      solutionFolderPath,
+      "MixedClassLibrary.sln"
+    );
     await addSolution(app, solutionPath);
     await app.client.refresh();
-    const results = await runner.runThroughSolution(solutionPath, "inplace", "net6.0", false, false);
-    await runner.validateHighLevelResults(results, ["0 of 2", "6 of 7", "14 of 46", "0", "(9)"]);
-    const projectFile: string = path.join(solutionFolderPath, "MixedClassLibrary", "VbClassLibrary.vbproj");
-    expect((await fs.readFile(projectFile)).indexOf("BouncyCastle.NetCore")).not.toBe(-1);
+    const results = await runner.runThroughSolution(
+      solutionPath,
+      "inplace",
+      "net6.0",
+      false,
+      false
+    );
+    await runner.validateHighLevelResults(results, [
+      "0 of 2",
+      "6 of 7",
+      "14 of 46",
+      "0",
+      "(9)",
+    ]);
+    const projectFile: string = path.join(
+      solutionFolderPath,
+      "MixedClassLibrary",
+      "VbClassLibrary.vbproj"
+    );
+    expect(
+      (await fs.readFile(projectFile)).indexOf("BouncyCastle.NetCore")
+    ).not.toBe(-1);
   });
 });
