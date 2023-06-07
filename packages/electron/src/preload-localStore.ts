c043c95aa5ae820ea4d7400d00b4c1c8d67c1584
Jiayu Wang <wwangjy@amazon.com>
2023-06-06 20:28:23 -0700

    chore: update version 1.9.5
    cr: https://code.amazon.com/reviews/CR-93356389

--- packages/electron/src/preload-localStore.ts
+++ packages/electron/src/preload-localStore.ts
@@ -139,7 +139,7 @@ const createReducerCacheStore = () =>
         "solution-reducer"
       )),
     },
-    name: "reducer-cache-1.9.4",
+    name: "reducer-cache-1.9.5",
     clearInvalidConfig: true,
     cwd:
       process.env["NODE_ENV"] === "test"
